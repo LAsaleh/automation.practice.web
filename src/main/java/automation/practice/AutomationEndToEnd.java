d7541fe64b70f6938886e4eb31d13d37e689992f
java-object-constructor <LAsaleh>
2022-05-24 01:42:09 -0400

    Automation.practice.web

--- src/main/java/automation/practice/AutomationEndToEnd.java
+++ src/main/java/automation/practice/AutomationEndToEnd.java
@@ -7,9 +7,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import static automation.practice.CommonUtils.*;
 import java.util.Locale;
 
 public class AutomationEndToEnd {
@@ -18,38 +20,40 @@ public class AutomationEndToEnd {
 
     public static void main(String[] args) {
 
+
         WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
+        driver.get(APP_URL);
+
+
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         wait.until((ExpectedCondition<Boolean>) wd -> ((JavascriptExecutor) wd).
                 executeScript("return document.readyState").equals("complete"));
 
 
-        driver.get(APP_URL);
-
         String title = driver.getTitle();
 
-        if(title.equals("My Store")){
+        if (title.equals("My Store")) {
             System.out.println("Title is correct");
-        }else{
+        } else {
             System.out.println("Title is not correct");
         }
 
         WebElement signInLink = driver.findElement(By.linkText("Sign in"));
 
-        if(signInLink.isDisplayed()){
+        if (signInLink.isDisplayed()) {
             signInLink.click();
             System.out.println("Clicking sign in link, because it exists");
-        }else{
+        } else {
             System.out.println("Sign in link does not exist on the page");
         }
 
 
-        String emailAddress= CommonUtils.randomEmail();
+        String emailAddress = randomEmail();
         System.out.println("Random email was generated" + emailAddress);
 
         WebElement newUserEmailInput = driver.findElement(By.id("email_create"));
@@ -58,38 +62,38 @@ public class AutomationEndToEnd {
 
 
         WebElement createAccountBtn = driver.findElement(By.id("SubmitCreate"));
-        if(createAccountBtn.isEnabled()){
+        if (createAccountBtn.isEnabled()) {
             createAccountBtn.click();
             System.out.println("Create account button is clicked");
-        }else{
+        } else {
             System.out.println("Create account button isn't clicked");
         }
 
         title = driver.getTitle();
-        if(title.equals("Login - My Store")){
+        if (title.equals("Login - My Store")) {
             System.out.println("Title is correct");
-        }else{
+        } else {
             System.out.println("Title is not correct");
         }
 
         WebElement mainHeader = driver.findElement(By.className("page-subheading"));
-        if(mainHeader.getText().equals("CREATE AN ACCOUNT")){
+        if (mainHeader.getText().equals("CREATE AN ACCOUNT")) {
             System.out.println("Main header is correct");
-        }else {
+        } else {
             System.out.println("Main header is not correct");
         }
 
-        int randomGender = (int)(Math.random()*(2 - 0) + 0);
-        WebElement genderRadioBtn = driver.findElement(By.xpath("//input[@id='id_gender"+randomGender+"']"));
+        int randomGender = (int) (Math.random() * (2 - 0) + 0); // I can also call the Method by its class so[ int randomGander = CommonUtils.randomNumber(1,2);]
+        WebElement genderRadioBtn = driver.findElement(By.xpath("//input[@id='id_gender" + randomGender + "']"));
         //click mr or mrs randomly
         genderRadioBtn.click();
 
 
         int atSign = emailAddress.indexOf("@");
         String[] fullName = emailAddress.substring(0, atSign).split("\\.");
-        String firstName = fullName[1].substring(0,1).toLowerCase() + fullName[1].substring(1);
-        String lastName = fullName[0].substring(0,1).toLowerCase() + fullName[1].substring(1);
-        String password = lastName + "124" + firstName.charAt(0 ) + "!";
+        String firstName = fullName[1].substring(0, 1).toLowerCase() + fullName[1].substring(1);
+        String lastName = fullName[0].substring(0, 1).toLowerCase() + fullName[1].substring(1);
+        String password = lastName + "124" + firstName.charAt(0) + "!";
 
         WebElement inputCustomerFirstNM = driver.findElement(By.id("customer_firstname"));
         inputCustomerFirstNM.sendKeys(firstName);
@@ -101,14 +105,105 @@ public class AutomationEndToEnd {
         emailInputRegistration.getAttribute("value");
 
         //compare the randomly generated  email with populated email on the register page
-        if(emailInputRegistration.equals((emailAddress))){
+        if (emailInputRegistration.equals((emailAddress))) {
             System.out.println("Email ids are correct");
-        }else {
+        } else {
             System.out.println("Email ids are not correct");
         }
 
         WebElement passwdInput = driver.findElement(By.id("passwd"));
         passwdInput.sendKeys(password);
+
+        String dob = randomDOBAbove18();
+        String[] splitDob = dob.split("-");
+        int splitDobYear = Integer.parseInt(splitDob[0]);
+        int splitDobMonth = Integer.parseInt(splitDob[1]);
+        int splitDobDay = Integer.parseInt(splitDob[2]);
+
+
+        WebElement days = driver.findElement(By.id("days"));
+        WebElement months = driver.findElement(By.id("months"));
+        WebElement years = driver.findElement(By.id("years"));
+
+        Select select = new Select(days);
+        select.selectByValue(String.valueOf(splitDobDay));
+
+        select = new Select(months);
+        select.selectByValue(String.valueOf(splitDobMonth));
+
+        select = new Select(years);
+        select.selectByValue(String.valueOf(splitDobYear));
+
+
+        WebElement newsletterBX = driver.findElement(By.id("newsletter"));
+        WebElement specialOffersBx = driver.findElement(By.id("optin"));
+
+        if (!newsletterBX.isSelected()) {
+            System.out.println("Checked newsletter checkbox");
+            newsletterBX.click();
+
+        } else {
+            System.out.println("Checked newsletter is already checkbox");
+
+        }
+        if (!specialOffersBx.isSelected()) {
+            System.out.println("Checked special offers checkbox");
+            specialOffersBx.click();
+
+        } else {
+            System.out.println("Checked special offers is already checkbox");
+
+
+            WebElement addrsFirstName = driver.findElement(By.id("firstname"));
+            String valueGetFName = addrsFirstName.getAttribute("value");
+            if (valueGetFName.equals(firstName)){
+                System.out.println("First name is correct");
+            }else {
+                System.out.println("First name is NOT correct in address first name input field");
+            }
+
+
+            WebElement addrsLastName = driver.findElement(By.id("lastname"));
+            String valueGetLName = addrsLastName.getAttribute("value");
+            if (valueGetLName.equals(lastName)){
+                System.out.println("Last name is correct");
+            }else{
+                System.out.println("Last name is NOT correct in address first name input field");
+            }
+
+
+            // RandomDate rd = new RandomDate(LocalDate.of(1900, 1, 1), LocalDate.of(2010, 1, 1));
+        }
+
+        //company
+        WebElement companyNames = driver.findElement(By.id("company"));
+        String randomCompanyName = randomCompanyName();
+        companyNames.sendKeys(randomCompanyName);
+
+        //address
+        WebElement address1 = driver.findElement(By.id("address1"));
+        String randomAddr = randomStreetAddress();
+        address1.sendKeys(randomAddr);
+
+
+        WebElement city = driver.findElement(By.id("city"));
+        String randomCityX = randomCity();
+        city.sendKeys(randomCityX);
+
+        WebElement id_state = driver.findElement(By.id("id_state"));
+        select = new Select(id_state);
+        String valueRandomState = randomState();
+        select.selectByVisibleText(valueRandomState);
+
+
+        WebElement additionalInfo = driver.findElement(By.id("other"));
+        String randomAdditional = generateRandomString(100);
+        additionalInfo.sendKeys(randomAdditional);
+
+
+        WebElement phone_mobile = driver.findElement(By.id("phone_mobile"));
+        String randomChecker = randomPhoneNumber();
+        phone_mobile.sendKeys(randomChecker);
 
 
 
--- src/main/java/automation/practice/CommonUtils.java
+++ src/main/java/automation/practice/CommonUtils.java
@@ -2,9 +2,68 @@ package automation.practice;
 
 import com.github.javafaker.Faker;
 
+import java.time.LocalDate;
 import java.util.Locale;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class CommonUtils {
+    public class CommonUtils {
+
+        public static String  randomPhoneNumber(){
+            return String.format("(%03d) %03d-%04d",
+                    (int) Math.floor(999*Math.random()),
+                    (int) Math.floor(999*Math.random()),
+                    (int) Math.floor(9999*Math.random()));
+        }
+
+
+
+
+        public static String generateRandomString(int limit){
+            String saltChars = "qwertyuiopasdfghjklzxcvbnm1234567890./";
+            StringBuilder salt = new StringBuilder();
+            Random rnd = new Random();
+            while (salt.length() < limit){
+                int index = (int) (rnd.nextFloat() * saltChars.length());
+                salt.append(saltChars.charAt(index));
+            }
+            String saltStr = salt.toString();
+            return saltStr;
+
+
+        }
+
+
+
+        public static String randomState(){
+            return new Faker().address().state();
+        }
+
+
+public static String randomCompanyName(){
+            return new Faker().company().name();
+}
+
+    public static String randomCity() {
+        return new Faker().address().cityName();
+
+
+}
+
+    public static String randomStreetAddress(){
+        return new Faker().address().streetAddress();
+
+
+
+}
+
+
+    public static int randomNumber(int from , int to ){
+        return (int)(Math.random()* (to - from + 1)+ from);
+    }
+
+
+
     public static String randomEmail(){
         Faker faker = new Faker();
         String lastName = faker.name().lastName();
@@ -17,20 +76,37 @@ public class CommonUtils {
 
     }
 
+    public static String randomDOBAbove18 () {
+
+        LocalDate startDate = LocalDate.of(1950, 1, 1);
+        long start = startDate.toEpochDay();
+
+        LocalDate endDate = LocalDate.of(LocalDate.now().getYear() - 18, 1, 1);
+        long end = endDate.toEpochDay();
+
+        long randomEpochDay = ThreadLocalRandom.current().longs(start, end).findAny().getAsLong();
+
+        return LocalDate.ofEpochDay(randomEpochDay).toString();
+    }
+
+
     public static void main(String[] args) {
 
-        String email = "wilkinson.edgar@hotmail.com";
-
-        int atSign = email.indexOf("@");
-        String[] fullName = email.substring(0, atSign).split("\\.");
-        String firstName = fullName[1].substring(0,1).toLowerCase() + fullName[1].substring(1);
-        String lastName = fullName[0].substring(0,1).toLowerCase() + fullName[1].substring(1);
-
-
-        System.out.println(firstName);
-        System.out.println(lastName);
-
-
+//        String email = "wilkinson.edgar@hotmail.com";
+//
+//        int atSign = email.indexOf("@");
+//        String[] fullName = email.substring(0, atSign).split("\\.");
+//        String firstName = fullName[1].substring(0,1).toLowerCase() + fullName[1].substring(1);
+//        String lastName = fullName[0].substring(0,1).toLowerCase() + fullName[1].substring(1);
+//
+//
+//        System.out.println(firstName);
+//        System.out.println(lastName);
+//
+//        System.out.println(randomNumber(1,30));
+        System.out.println(randomDOBAbove18());
+        System.out.println(generateRandomString(100));
+        System.out.println(randomPhoneNumber());
 
     }
 
